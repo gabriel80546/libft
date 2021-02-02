06d28f4095d288c88b22762ca1cc96088b7d19ae
Gabriel Passos <gabriel80546@gmail.com>
2021-02-02 14:04:42 -0300

    eae

--- testes/bzero.sh
+++ testes/bzero.sh
@@ -1,1 +1,6 @@
-gcc bzero.c ../libft.h ../ft_memset.c ../ft_bzero.c -o main
+if [ $(whoami) = "gabriel" ]
+then
+    gcc bzero.c ../libft.h ../ft_memset.c ../ft_bzero.c -o main
+else
+    gcc bzero.c ../libft.h ../ft_memset.c ../ft_bzero.c
+fi
