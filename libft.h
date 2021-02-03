04216b9e09399d3fb9e928d43b48cd3039f63d03
Gabriel Passos <gabriel80546@gmail.com>
2021-02-03 09:18:28 -0300

    libft header

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 09:35:11 by gabriel           #+#    #+#             */
-/*   Updated: 2021/02/03 08:58:26 by gabriel          ###   ########.fr       */
+/*   Updated: 2021/02/03 09:17:08 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,20 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int     ft_memcmp(const void *s1, const void *s2, size_t n);
+size_t	strlen(const char *s);
+char	*strchr(const char *s, int c);
+char	*strrchr(const char *s, int c);
+int		strncmp(const char *s1, const char *s2, size_t n);
+int		atoi(const char *nptr);
+int		isalpha(int c);
+int		isdigit(int c);
+int		isalnum(int c);
+int		isascii(int c);
+int		isprint(int c);
+int		toupper(int c);
+int		tolower(int c);
+void	*calloc(size_t nmemb, size_t size);
+char	*strdup(const char *s);
+
 
 #endif
