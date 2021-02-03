43d06c45eca90e6a22f51a95a36b2a4e6ac1c5ca
Gabriel Passos <gabriel80546@gmail.com>
2021-02-03 09:07:43 -0300

    norma

--- testes/memcmp.c
+++ testes/memcmp.c
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 08:50:50 by gabriel           #+#    #+#             */
-/*   Updated: 2021/02/03 08:58:12 by gabriel          ###   ########.fr       */
+/*   Updated: 2021/02/03 09:06:58 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,32 +16,25 @@
 
 int	main(void)
 {
-	char str1[15];
-	char str2[15];
-	int ret;
+	char	str1[15];
+	char	str2[15];
+	int		ret;
 
 	memcpy(str1, "abcdef", 6);
 	memcpy(str2, "ABCDEF", 6);
-
 	ret = memcmp(str1, str2, 5);
-
-	if(ret > 0) {
+	if (ret > 0)
 		printf("str2 is less than str1\n");
-	} else if(ret < 0) {
+	else if (ret < 0)
 		printf("str1 is less than str2\n");
-	} else {
+	else
 		printf("str1 is equal to str2\n");
-	}
-
 	ret = ft_memcmp(str1, str2, 5);
-
-	if(ret > 0) {
+	if (ret > 0)
 		printf("str2 is less than str1\n");
-	} else if(ret < 0) {
+	else if (ret < 0)
 		printf("str1 is less than str2\n");
-	} else {
+	else
 		printf("str1 is equal to str2\n");
-	}
-
-	return(0);
+	return (0);
 }
