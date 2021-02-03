09f0987e74f63133377b05258d3ae6155961499d
Gabriel Passos <gabriel80546@gmail.com>
2021-02-03 09:04:15 -0300

    memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/03 08:48:24 by gabriel           #+#    #+#             */
+/*   Updated: 2021/02/03 09:03:34 by gabriel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	int	contador;
+
+	contador = 0;
+	while (contador < n)
+	{
+		if(*((char *)s1 + contador) != *((char *)s2 + contador))
+			return (*((char *)s1 + contador) - *((char *)s2 + contador));
+		contador++;
+	}
+	return 0;
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 09:35:11 by gabriel           #+#    #+#             */
-/*   Updated: 2021/02/03 08:26:35 by gabriel          ###   ########.fr       */
+/*   Updated: 2021/02/03 08:58:26 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+int     ft_memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
