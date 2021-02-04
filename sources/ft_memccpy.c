9497e3163bf86684a3edc805e0a2bb221cbe479e
Gabriel Passos <gabriel80546@gmail.com>
2021-02-04 09:03:51 -0300

    antes de alterar memccpy

--- sources/ft_memccpy.c
+++ sources/ft_memccpy.c
@@ -6,11 +6,22 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 14:17:00 by gabriel           #+#    #+#             */
-/*   Updated: 2021/02/03 14:03:02 by gabriel          ###   ########.fr       */
+/*   Updated: 2021/02/04 09:03:31 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+// teste 2
+//  mem = jjjjjjjjjjjjjjjjjjjjjjjjjjjjjj <= tava assim
+// (mem, "zyxwvutsrqponmlkjihgfedcba", 'r', 3) <= argumentos passados
+//  mem = (NULL) <= deveria retornar isso
+//  mem = (NULL) <= retornei isso
+// teste 3
+//  mem = jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjyxwvutsrzyxjjjjjjjjjjjjjjjjjjjjjjjjjjj <= tava assim
+// (mem, "zyxwvutsrqponmlkjihgfedcba", 'a', 26) <= argumentos passados
+//  mem = jjjjyxwvutsrzyxjjjjjjjjjjjjjjjjjjjjjjjjjjj <= deveria retornar isso
+//  mem = (NULL) <= retornei isso
 
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
@@ -20,9 +31,9 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 	while (contador < n)
 	{
 		if ((*((char *)src + contador - 1)) == c)
-			break ;
+			return (dest + contador);
 		*((char *)dest + contador) = *((char *)src + contador);
 		contador++;
 	}
-	return (dest);
+	return (NULL);
 }
--- testes/memccpy.c
+++ testes/memccpy.c
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 07:23:02 by gabriel           #+#    #+#             */
-/*   Updated: 2021/02/03 07:29:17 by gabriel          ###   ########.fr       */
+/*   Updated: 2021/02/04 09:02:22 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,127 @@ int	main(void)
 	memset(buffer, '\0', 80);
 	ft_memccpy(buffer, g_msg, ':', 80);
 	printf("%s\n", buffer);
+
+
+	void	*mem;
+	void	*copia;
+
+	mem = (void *)malloc(30);
+	copia = mem;
+
+	if(mem == NULL) { mem = copia; }
+	memset(mem, 'j', 30);
+	printf("%s\n", (mem == 0) ? "(NULL)" : (char *)mem);
+	if(mem == NULL) { mem = copia; }
+	memset(mem, 'j', 30);
+	mem = memccpy(mem, "zyxwvutsrqponmlkjihgfedcba", 'r', 20);
+	printf("%s\n", (mem == 0) ? "(NULL)" : (char *)mem);
+	if(mem == NULL) { mem = copia; }
+	memset(mem, 'j', 30);
+	mem = memccpy(mem, "zyxwvutsrqponmlkjihgfedcba", 'r', 3);
+	printf("%s\n", (mem == 0) ? "(NULL)" : (char *)mem);
+	if(mem == NULL) { mem = copia; }
+	memset(mem, 'j', 30);
+	mem = memccpy(mem, "zyxwvutsrqponmlkjihgfedcba", 'a', 26);
+	printf("%s\n", (mem == 0) ? "(NULL)" : (char *)mem);
+	if(mem == NULL) { mem = copia; }
+	memset(mem, 'j', 30);
+	mem = memccpy(mem, "zyxwvuzyxwvu", 'x', 20);
+	printf("%s\n", (mem == 0) ? "(NULL)" : (char *)mem);
+	if(mem == NULL) { mem = copia; }
+	memset(mem, 'j', 30);
+
+
+	printf("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+	printf("$@##@#@#@##@#@#@#@#@#@#@###@@@#@#@#@#@#@#@#@#@#@#@$\n");
+	printf("$#@#@#@#@#@#@#@#@#@#@#@ Parte 2 @#@#@#@#@#@#@#@#@#$\n");
+	printf("$#@@@#@#@#@@@@##@#@#@#@#@##@@@@#@#@#@#@@@@#@#@@@@#$\n");
+	printf("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+
+
+
+	if(mem == NULL) { mem = copia; }
+	memset(mem, 'j', 30);
+	printf("%s\n", (mem == 0) ? "(NULL)" : (char *)mem);
+	if(mem == NULL) { mem = copia; }
+	memset(mem, 'j', 30);
+	mem = memccpy(mem, "zyxwvutsrqponmlkjihgfedcba", 'r', 20);
+	printf("%s\n", (mem == 0) ? "(NULL)" : (char *)mem);
+	if(mem == NULL) { mem = copia; }
+	memset(mem, 'j', 30);
+	mem = memccpy(mem, "zyxwvutsrqponmlkjihgfedcba", 'r', 3);
+	printf("%s\n", (mem == 0) ? "(NULL)" : (char *)mem);
+	if(mem == NULL) { mem = copia; }
+	memset(mem, 'j', 30);
+	mem = memccpy(mem, "zyxwvutsrqponmlkjihgfedcba", 'a', 26);
+	printf("%s\n", (mem == 0) ? "(NULL)" : (char *)mem);
+	if(mem == NULL) { mem = copia; }
+	memset(mem, 'j', 30);
+	mem = memccpy(mem, "zyxwvuzyxwvu", 'x', 20);
+	printf("%s\n", (mem == 0) ? "(NULL)" : (char *)mem);
+	if(mem == NULL) { mem = copia; }
+	memset(mem, 'j', 30);
+
+	printf("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+	printf("$@##@#@#@##@#@#@#@#@#@#@###@@@#@#@#@#@#@#@#@#@#@#@$\n");
+	printf("$#@#@#@#@#@#@#@#@#@#@#@ Parte 3 @#@#@#@#@#@#@#@#@#$\n");
+	printf("$#@@@#@#@#@@@@##@#@#@#@#@##@@@@#@#@#@#@@@@#@#@@@@#$\n");
+	printf("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+
+
+	if(mem == NULL) { mem = copia; }
+	memset(mem, 'j', 30);
+	printf("%s\n", (mem == 0) ? "(NULL)" : (char *)mem);
+	if(mem == NULL) { mem = copia; }
+	memset(mem, 'j', 30);
+	mem = memccpy(mem, "zyxwvutsrqponmlkjihgfedcba", 'r', 20);
+	printf("%s\n", (mem == 0) ? "(NULL)" : (char *)mem);
+	if(mem == NULL) { mem = copia; }
+	memset(mem, 'j', 30);
+	mem = memccpy(mem, "zyxwvutsrqponmlkjihgfedcba", 'r', 3);
+	printf("%s\n", (mem == 0) ? "(NULL)" : (char *)mem);
+	if(mem == NULL) { mem = copia; }
+	memset(mem, 'j', 30);
+	mem = memccpy(mem, "zyxwvutsrqponmlkjihgfedcba", 'a', 26);
+	printf("%s\n", (mem == 0) ? "(NULL)" : (char *)mem);
+	if(mem == NULL) { mem = copia; }
+	memset(mem, 'j', 30);
+	mem = memccpy(mem, "zyxwvuzyxwvu", 'x', 20);
+	printf("%s\n", (mem == 0) ? "(NULL)" : (char *)mem);
+	if(mem == NULL) { mem = copia; }
+	memset(mem, 'j', 30);
+
+	printf("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+	printf("$@##@#@#@##@#@#@#@#@#@#@###@@@#@#@#@#@#@#@#@#@#@#@$\n");
+	printf("$#@#@#@#@#@#@#@#@#@#@#@ Parte 4 @#@#@#@#@#@#@#@#@#$\n");
+	printf("$#@@@#@#@#@@@@##@#@#@#@#@##@@@@#@#@#@#@@@@#@#@@@@#$\n");
+	printf("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+
+
+	if(mem == NULL) { mem = copia; }
+	memset(mem, 'j', 30);
+	printf("%s\n", (mem == 0) ? "(NULL)" : (char *)mem);
+	if(mem == NULL) { mem = copia; }
+	memset(mem, 'j', 30);
+	mem = ft_memccpy(mem, "zyxwvutsrqponmlkjihgfedcba", 'r', 20);
+	printf("%s\n", (mem == 0) ? "(NULL)" : (char *)mem);
+	if(mem == NULL) { mem = copia; }
+	memset(mem, 'j', 30);
+	mem = ft_memccpy(mem, "zyxwvutsrqponmlkjihgfedcba", 'r', 3);
+	printf("%s\n", (mem == 0) ? "(NULL)" : (char *)mem);
+	if(mem == NULL) { mem = copia; }
+	memset(mem, 'j', 30);
+	printf("antes tava assim => (%s)\n", (mem == 0) ? "(NULL)" : (char *)mem);
+	mem = ft_memccpy(mem, "zyxwvutsrqponmlkjihgfedcba", 'a', 26);
+	printf("%s\n", (mem == 0) ? "(NULL)" : (char *)mem);
+	if(mem == NULL) { mem = copia; }
+	memset(mem, 'j', 30);
+	mem = ft_memccpy(mem, "zyxwvuzyxwvu", 'x', 20);
+	printf("%s\n", (mem == 0) ? "(NULL)" : (char *)mem);
+	if(mem == NULL) { mem = copia; }
+	memset(mem, 'j', 30);
+
 	return (0);
 }
+
+// printf("antes tava assim => (%s)\n", (mem == 0) ? "(NULL)" : (char *)mem);
