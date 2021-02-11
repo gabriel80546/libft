3b5cfea01ed55e53aaaa30fbfd423be64bf3075b
Gabriel Passos <gabriel80546@gmail.com>
2021-02-11 12:25:16 -0300

    ft_putnbr_fd

--- sources/ft_putnbr_fd.c
+++ sources/ft_putnbr_fd.c
@@ -6,16 +6,49 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/08 09:42:41 by gabriel           #+#    #+#             */
-/*   Updated: 2021/02/08 09:51:58 by gabriel          ###   ########.fr       */
+/*   Updated: 2021/02/11 12:24:30 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+#include <stdio.h>
 
-void	ft_putnbr_fd(int n, int fd)
+static void	ft_putnbr_fd_p(char numeros[40], int fd)
 {
-	char *nstr;
+	int i;
 
-	nstr = ft_itoa(n);
-	ft_putstr_fd(nstr, fd);
+	i = 0;
+	while (i < 40)
+	{
+		if (numeros[i] != -1)
+			ft_putchar_fd(numeros[i] + '0', fd);
+		i++;
+	}
+}
+
+void		ft_putnbr_fd(int n, int fd)
+{
+	char	numeros[40];
+	int		i;
+
+	if (n == -2147483648LL)
+	{
+		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
+	ft_memset(numeros, -1, 40);
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n = -n;
+	}
+	i = 39;
+	while (n > 9)
+	{
+		numeros[i] = (n % 10);
+		n = (n / 10);
+		i--;
+	}
+	numeros[i] = n;
+	ft_putnbr_fd_p(numeros, fd);
 }
