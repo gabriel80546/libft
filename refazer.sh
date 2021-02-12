3bb289e3803e3028eff3e2397d52c4d4e94e0839
Gabriel Passos <gabriel80546@gmail.com>
2021-02-12 14:53:31 -0300

    scripts

---
+++ refazer.sh
@@ -0,0 +1,25 @@
+caminho=/home/gabriel/Desktop/qd/projetos/libft/teste
+rm -rf -- $caminho
+cp -r -v -n /home/gabriel/Desktop/qd/projetos/libft/libft/ $caminho
+
+if [ "$1" = "1" ]
+then
+    echo primeiro teste
+elif [ "$1" = "2" ]
+then
+    cat $caminho/Makefile | sed 's/CAMINHO = sources\//CAMINHO = /g' > $caminho/oxi
+    mv $caminho/oxi $caminho/Makefile
+    cd $caminho
+    $caminho/change.sh
+    cd $2
+elif [ "$1" = "3" ]
+then
+    cat $caminho/Makefile | sed 's/CAMINHO = sources\//CAMINHO = /g' > $caminho/oxi
+    mv $caminho/oxi $caminho/Makefile
+    cd $caminho
+    $caminho/change.sh
+    cd $2
+else
+    echo so existem 3 testadores carai
+fi
+
