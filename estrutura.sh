ab9948ee2733f0f17f23cdcc63cd9c10b42285c3
gabriel80546 <gabriel80546@gmail.com>
2021-02-13 11:19:08 -0300

    estrutura.sh

--- estrutura.sh
+++ estrutura.sh
@@ -1,4 +1,4 @@
-base=/Users/gpassos-
+base=$(pwd)
 mkdir unit_test_two
 mkdir unit_test_three
 cp libft/refazer.sh .
@@ -8,3 +8,9 @@ cp libft/teste3.txt .
 git clone https://github.com/jtoty/Libftest
 git clone https://github.com/alelievr/libft-unit-test unit_test_two/libft-unit-test
 git clone https://github.com/ska42/libft-war-machine unit_test_three/libft-war-machine
+bose=$(echo $base | sed 's/\//\\\//g')
+echo "base = \"$base\""
+echo "bose = \"$bose\""
+cat refazer.sh | sed "s/base=.*$/base=$bose/g" > oxi
+mv oxi refazer.sh
+chmode 775 refazer.sh
