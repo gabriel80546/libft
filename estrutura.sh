ecb926de9059ac56b1db3f5f34223f289540a235
gabriel80546 <gabriel80546@gmail.com>
2021-02-13 10:21:58 -0300

    estrutura.sh

--- estrutura.sh
+++ estrutura.sh
@@ -1,4 +1,9 @@
 base=/Users/gpassos-
 mkdir unit_test_two
 mkdir unit_test_three
-echo "base = \"$base\""
+cp libft/refazer.sh .
+cp libft/teste1.txt .
+cp libft/teste2.txt .
+cp libft/teste3.txt .
+git clone https://github.com/jtoty/Libftest
+git clone https://github.com/alelievr/libft-unit-test unit_test_two/libft-unit-test
