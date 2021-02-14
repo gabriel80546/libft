9220ac737055fda9c47d68401e23f98feb3592c2
gabriel <gabriel80546@gmail.com>
2021-02-14 14:50:30 -0300

    adicionado mais testes

--- testes/memccpy.c
+++ testes/memccpy.c
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 07:23:02 by gabriel           #+#    #+#             */
-/*   Updated: 2021/02/04 09:29:32 by gabriel          ###   ########.fr       */
+/*   Updated: 2021/02/14 14:49:27 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,37 @@
 #include <stdlib.h>
 #include "../libft.h"
 
+int	main_old(void);
+
 char *g_msg = "This is the string: not copied";
 
-int	main(void)
+int					main(int argc, const char *argv[])
+{
+	int		arg;
+
+	char dest[100];
+	memset(dest, 'A', 100);
+
+	alarm(5);
+	if (argc == 1)
+		return (0);
+	if ((arg = atoi(argv[1])) == 1)
+		main_old();
+	else if (arg == 2)
+	{
+		printf("saida1 ft_memccpy = %p\n", ft_memccpy(dest, "coucou", 'c', 10));
+		printf("saida2 ft_memccpy = %p\n", (dest + 1));
+	}
+	else if (arg == 3)
+	{
+		printf("saida1 memccpy = %p\n", memccpy(dest, "coucou", 'c', 10));
+		printf("saida2 memccpy = %p\n", (dest + 1));
+	}
+	return (0);
+}
+
+
+int	main_old(void)
 {
 	char buffer[80];
 
