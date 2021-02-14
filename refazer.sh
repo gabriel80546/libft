f3d6b7b661d169645c8b6bdd1ae90a68adcd59b5
gabriel <gabriel80546@gmail.com>
2021-02-14 13:28:30 -0300

    nova estrutura

--- refazer.sh
+++ refazer.sh
@@ -5,18 +5,26 @@ cp -r -v -n $base/libft/ $caminho
 
 if [ "$1" = "1" ]
 then
-    echo primeiro teste
+    cd $caminho
+    $caminho/change.sh
+    cd $2
 elif [ "$1" = "2" ]
 then
-    cat $caminho/Makefile | sed 's/CAMINHO = sources\//CAMINHO = /g' > $caminho/oxi
-    mv $caminho/oxi $caminho/Makefile
     cd $caminho
     $caminho/change.sh
     cd $2
 elif [ "$1" = "3" ]
 then
-    cat $caminho/Makefile | sed 's/CAMINHO = sources\//CAMINHO = /g' > $caminho/oxi
-    mv $caminho/oxi $caminho/Makefile
+    cd $caminho
+    $caminho/change.sh
+    cd $2
+elif [ "$1" = "4" ]
+then
+    cd $caminho
+    $caminho/change.sh
+    cd $2
+elif [ "$1" = "5" ]
+then
     cd $caminho
     $caminho/change.sh
     cd $2
