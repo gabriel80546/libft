51da6ae08f2ff2e64eb0d1aec3d9b63d271e4258
Gabriel <gabriel80546@gmail.com>
2021-02-16 18:06:49 -0300

    ft_strlcpy protegida

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 09:32:13 by gabriel           #+#    #+#             */
-/*   Updated: 2021/02/12 13:53:45 by gabriel          ###   ########.fr       */
+/*   Updated: 2021/02/16 18:05:45 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,24 @@
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	contador;
-	char	atual;
 	int		zero;
 
+	if (dst == NULL || src == NULL)
+		return (0);
 	if (size == 0)
 		return (ft_strlen(src));
 	zero = 0;
 	contador = 0;
-	atual = *((char *)src + contador);
-	while (atual != '\0')
+	while (*((char *)src + contador) != '\0')
 	{
 		if (contador < size)
-			*((char *)dst + contador) = atual;
+			*((char *)dst + contador) = *((char *)src + contador);
 		else if (contador == size)
 		{
 			zero = 1;
 			*((char *)dst + contador - 1) = '\0';
 		}
 		contador++;
-		atual = *((char *)src + contador);
 	}
 	if (zero == 0)
 		*((char *)dst + contador) = '\0';
