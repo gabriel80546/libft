d2453e30af408085fde60e01cee430585dc7c9b0
gabriel <gabriel80546@gmail.com>
2021-02-17 15:04:24 -0300

    novo

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -3,13 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gpassos- <gpassos-@student.42sp.org.br>       +#+  +:+       +#+        */
+/*   By: gpassos- <gpassos-@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/17 10:17:53 by gpassos-          #+#    #+#             */
-/*   Updated: 2021/02/17 10:17:58 by gpassos-         ###   ########.fr       */
+/*   Created: 2021/02/17 10:59:22 by gpassos-          #+#    #+#             */
+/*   Updated: 2021/02/17 10:59:23 by gpassos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
